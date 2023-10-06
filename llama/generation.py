bb4651673b3ef90f5196e63950ce32fbd0711080
Charles Jin <ccj@csail.mit.edu>
2023-10-05 21:41:43 -0400

    make easier to use as a package

--- llama/generation.py
+++ llama/generation.py
@@ -16,8 +16,8 @@ from fairscale.nn.model_parallel.initialize import (
     model_parallel_is_initialized,
 )
 
-from llama.model import ModelArgs, Transformer
-from llama.tokenizer import Tokenizer
+from .model import ModelArgs, Transformer
+from .tokenizer import Tokenizer
 
 Role = Literal["system", "user", "assistant"]
 
